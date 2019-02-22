efbbe56a095881e2e20b8eeaf4650d6a0512b8b4
Murilo Henrique <oofleaoo@gmail.com>
2019-02-22 00:23:06 -0300

    adicionando funcionalidades para o registro de horario

---
+++ modulo2/src/app/controllers/AvailableController.js
@@ -0,0 +1,8 @@
+const moment = require('moment')
+const AppointmentController = require('./AppointmentController')
+
+class AvailableController {
+  index (req, res) {}
+}
+
+module.exports = new AvailableController()
