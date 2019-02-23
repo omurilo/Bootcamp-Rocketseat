526dafa34108a8aecb94f5da086563c272950e3e
Murilo Henrique <oofleaoo@gmail.com>
2019-02-22 22:02:34 -0300

    adicionando algumas funcionalidades e preparando ambiente para desafio2

--- modulo2/src/app/controllers/DashboardController.js
+++ modulo2/src/app/controllers/DashboardController.js
@@ -1,10 +1,33 @@
-const { User } = require('../models')
+const { User, Appointment } = require('../models')
+const { Op } = require('sequelize')
+const moment = require('moment')
 
 class DashboardController {
   async index (req, res) {
     const providers = await User.findAll({ where: { provider: true } })
-
-    return res.render('dashboard', { providers })
+    const date = moment()
+    const appointments = await Appointment.findAll({
+      where: {
+        user_id: req.session.user.id,
+        date: {
+          [Op.gte]: [date.startOf('day').format()]
+        }
+      }
+    })
+    const appointmentsUser = []
+    appointments.forEach(appointment => {
+      providers.forEach(provider => {
+        if (appointment.provider_id === provider.id) {
+          appointmentsUser.push({
+            date: moment(appointment.date).format('DD/MM/YY'),
+            hour: moment(appointment.date).format('HH:mm'),
+            provider: provider.name,
+            avatar: provider.avatar
+          })
+        }
+      })
+    })
+    return res.render('dashboard', { providers, appointmentsUser })
   }
 }
 
