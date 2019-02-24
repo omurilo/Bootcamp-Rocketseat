4b9b9627d0e92c6be60ffa840057894a026e520d
Murilo Henrique <oofleaoo@gmail.com>
2019-02-24 14:14:31 -0300

    Desafio 2 refatorado

--- modulo2/src/app/controllers/DashboardController.js
+++ modulo2/src/app/controllers/DashboardController.js
@@ -5,24 +5,20 @@ const moment = require('moment')
 class DashboardController {
   async index (req, res) {
     const providers = await User.findAll({ where: { provider: true } })
-    const users = await User.findAll({ where: { provider: false } })
     const date = moment()
     const userId = req.session.user.id
     const appointments = await Appointment.findAll({
       where: {
+        user_id: userId,
         date: {
           [Op.gte]: [date.startOf('day').format()]
         }
       }
     })
     const appointmentsUser = []
-    const appointmentsProvider = []
     appointments.forEach(appointment => {
       providers.forEach(provider => {
-        if (
-          appointment.provider_id === provider.id &&
-          appointment.user_id === userId
-        ) {
+        if (appointment.provider_id === provider.id) {
           appointmentsUser.push({
             date: moment(appointment.date).format('DD/MM/YY'),
             hour: moment(appointment.date).format('HH:mm'),
@@ -31,25 +27,10 @@ class DashboardController {
           })
         }
       })
-      /* forEach para pegar os usuário que existem cadastrados para aquele provider */
-      users.forEach(user => {
-        if (
-          appointment.provider_id === userId &&
-          appointment.user_id === user.id
-        ) {
-          appointmentsProvider.push({
-            date: moment(appointment.date).format('DD/MM/YY'),
-            hour: moment(appointment.date).format('HH:mm'),
-            user: user.name,
-            avatar: user.avatar
-          })
-        }
-      })
     })
     return res.render('dashboard', {
       providers,
-      appointmentsUser,
-      appointmentsProvider
+      appointmentsUser
     })
   }
 }
