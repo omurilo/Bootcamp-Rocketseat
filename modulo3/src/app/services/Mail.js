4a36357aca67e9057265605d9efeb72e96d05e3a
Murilo Henrique <oofleaoo@gmail.com>
2019-02-25 20:25:41 -0300

    express-handlebars update config

--- modulo3/src/app/services/Mail.js
+++ modulo3/src/app/services/Mail.js
@@ -3,14 +3,17 @@ const mailConfig = require('../../config/mail')
 const path = require('path')
 const hbs = require('nodemailer-express-handlebars')
 const exphbs = require('express-handlebars')
+const viewPath = path.resolve(__dirname, '..', 'views', 'emails')
 
 const transport = nodemailer.createTransport(mailConfig)
 
 transport.use(
   'compile',
   hbs({
-    viewEngine: exphbs(),
-    viewPath: path.resolve(__dirname, '..', 'views', 'emails'),
+    viewEngine: exphbs.create({
+      partialsDir: path.resolve(viewPath, 'partials')
+    }),
+    viewPath,
     extName: '.hbs'
   })
 )
