994236cf00c8fe16e3e4d109b43a965e4f6e7d5a
Murilo Henrique <oofleaoo@gmail.com>
2019-02-24 22:46:51 -0300

    Sentry for production and .env add

--- modulo3/src/server.js
+++ modulo3/src/server.js
@@ -1,18 +1,26 @@
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const validate = require('express-validation')
+const Sentry = require('@sentry/node')
 const Youch = require('youch')
 const databaseConfig = require('./config/database')
+const SentryConfig = require('./config/sentry')
 
 class App {
   constructor () {
     this.express = express()
     this.isDev = process.env.NODE_ENV !== 'production'
 
+    this.sentry()
     this.database()
     this.middlewares()
     this.routes()
     this.exception()
+  }
+
+  sentry () {
+    Sentry.init({ dsn: SentryConfig.dsn })
   }
 
   database () {
@@ -24,6 +32,7 @@ class App {
 
   middlewares () {
     this.express.use(express.json())
+    this.express.use(Sentry.Handlers.requestHandler())
   }
 
   routes () {
@@ -31,6 +40,10 @@ class App {
   }
 
   exception () {
+    if (process.env.NODE_ENV === 'production') {
+      this.express.use(Sentry.Handlers.errorHandler())
+    }
+
     this.express.use(async (err, req, res, next) => {
       if (err instanceof validate.ValidationError) {
         return res.json(err)
