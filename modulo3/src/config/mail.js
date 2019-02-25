faccc8e31474e1f630ce2be7682f86e9e9883713
Murilo Henrique <oofleaoo@gmail.com>
2019-02-25 05:43:20 -0300

    mail.js with env variables not work

--- modulo3/src/config/mail.js
+++ modulo3/src/config/mail.js
@@ -1,9 +1,21 @@
-module.exports = {
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
-  secure: process.env.MAIL_SECURE,
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    secure: process.env.MAIL_SECURE,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS
+    }
+  }
+} else {
+  module.exports = {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+      user: '9294efd39f7748',
+      pass: '8e09eb606a7e32'
+    }
   }
 }
