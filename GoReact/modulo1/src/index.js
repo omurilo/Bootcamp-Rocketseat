3e077ddc55017e41371768d3419ba29ff6d85a2b
Murilo Henrique <oofleaoo@gmail.com>
2019-02-25 21:33:33 -0300

    State and more changes

--- GoReact/modulo1/src/index.js
+++ GoReact/modulo1/src/index.js
@@ -1,18 +1,39 @@
 import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
 import { render } from "react-dom";
 
 class Button extends Component {
+  static defaultProps = {
+    children: "Salvar"
+  };
+
+  static propTypes = {
+    onClick: PropTypes.func.isRequired,
+    children: PropTypes.string
+  };
+
   render() {
-    return <a href="">Salvar</a>;
+    return <button onClick={this.props.onClick}>{this.props.children}</button>;
   }
 }
 
 class App extends Component {
+  state = {
+    counter: 0
+  };
+
+  handleClick = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
+
   render() {
     return (
       <Fragment>
         <h1>Hello RocketSeat</h1>
-        <Button />
+        <h2>o valor da soma é: {this.state.counter}</h2>
+        <Button onClick={this.handleClick}>Somar</Button>
       </Fragment>
     );
   }
