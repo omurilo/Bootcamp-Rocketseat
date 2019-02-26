e8919b9c56323e9b2c9685942a983949f4aae755
Murilo Henrique <oofleaoo@gmail.com>
2019-02-25 21:05:19 -0300

    initial commit, react and babel and webpack configs

---
+++ GoReact/modulo1/src/index.js
@@ -0,0 +1,21 @@
+import React, { Component, Fragment } from "react";
+import { render } from "react-dom";
+
+class Button extends Component {
+  render() {
+    return <a href="">Salvar</a>;
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello RocketSeat</h1>
+        <Button />
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
