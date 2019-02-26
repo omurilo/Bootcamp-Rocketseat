e48d866614294a77423a27741d60816044e7b1db
Murilo Henrique <oofleaoo@gmail.com>
2019-02-25 22:25:23 -0300

    final commit eslint + prettier + babel-eslint configured

--- GoReact/modulo1/src/index.js
+++ GoReact/modulo1/src/index.js
@@ -1,47 +1,30 @@
-import React, { Component, Fragment } from "react";
-import { render } from "react-dom";
+import React, { Component, Fragment } from 'react';
+import { render } from 'react-dom';
 
-import Button from "./components/Button";
+import Button from './components/Button';
 
-import "./style.scss";
-import { red } from "ansi-colors";
+import './style.scss';
 
 class App extends Component {
   state = {
-    counter: 0
+    counter: 0,
   };
 
-  componentDidMount() {
-    /* Perfeiro para chamadas a api, cadastro de eventlisteners...*/
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    /* Alteração em uma propriedade ou estado esta função é chamada */
-    /* Utilizado para vetar a renderização caso as alterações não forem significativas*/
-    /* Muito utilizado para melhora de performance do componente */
-    return nextState.counter <= 10;
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    /* Manipulação de dados após o update do componente */
-  }
-
-  componentWillUnmount() {
-    /* Utilizado para limpar os eventlisteners */
-  }
-
   handleClick = () => {
+    const { counter } = this.state;
     this.setState({
-      counter: this.state.counter + 1
+      counter: counter + 1,
     });
   };
 
   render() {
+    const { counter } = this.state;
     return (
       <Fragment>
         <h1 className="title">Hello RocketSeat</h1>
-        <h2 style={{ color: "#f00" }}>
-          o valor da soma é: {this.state.counter}
+        <h2 style={{ color: '#f00' }}>
+          o valor da soma é:
+          {counter}
         </h2>
         <Button onClick={this.handleClick}>Somar</Button>
       </Fragment>
@@ -49,4 +32,4 @@ class App extends Component {
   }
 }
 
-render(<App />, document.getElementById("app"));
+render(<App />, document.getElementById('app'));
