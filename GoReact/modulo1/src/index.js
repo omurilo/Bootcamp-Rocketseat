fd9c8b6205e38431a69179e4c023192648b7fa7a
Murilo Henrique <oofleaoo@gmail.com>
2019-02-25 21:48:19 -0300

    lifecycle unmount update shouldupdate and mount

--- GoReact/modulo1/src/index.js
+++ GoReact/modulo1/src/index.js
@@ -7,6 +7,25 @@ class App extends Component {
     counter: 0
   };
 
+  componentDidMount() {
+    /* Perfeiro para chamadas a api, cadastro de eventlisteners...*/
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    /* Alteração em uma propriedade ou estado esta função é chamada */
+    /* Utilizado para vetar a renderização caso as alterações não forem significativas*/
+    /* Muito utilizado para melhora de performance do componente */
+    return nextState.counter <= 10;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    /* Manipulação de dados após o update do componente */
+  }
+
+  componentWillUnmount() {
+    /* Utilizado para limpar os eventlisteners */
+  }
+
   handleClick = () => {
     this.setState({
       counter: this.state.counter + 1
