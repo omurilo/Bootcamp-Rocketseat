d34f3ac9e30bbf816b72c8d6afc256a8ecc2159c
Murilo Henrique <oofleaoo@gmail.com>
2019-02-25 21:43:21 -0300

    stateless and statefull

--- GoReact/modulo1/src/index.js
+++ GoReact/modulo1/src/index.js
@@ -1,21 +1,6 @@
 import React, { Component, Fragment } from "react";
-import PropTypes from "prop-types";
 import { render } from "react-dom";
-
-class Button extends Component {
-  static defaultProps = {
-    children: "Salvar"
-  };
-
-  static propTypes = {
-    onClick: PropTypes.func.isRequired,
-    children: PropTypes.string
-  };
-
-  render() {
-    return <button onClick={this.props.onClick}>{this.props.children}</button>;
-  }
-}
+import Button from "./components/Button";
 
 class App extends Component {
   state = {
