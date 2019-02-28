4cba06574c0a9af4e879f1b6f67b5bac86017d00
Murilo Henrique <oofleaoo@gmail.com>
2019-02-27 23:10:41 -0300

    Arrumando Bagunça

--- modulo3/src/app/validators/Approve.js
+++
@@ -1,7 +0,0 @@
-const Joi = require('joi')
-
-module.exports = {
-  body: {
-    ad: Joi.string().required()
-  }
-}
--- modulo3/src/config/auth.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  secret: process.env.APP_SECRET,
-  ttl: 86400000
-}
